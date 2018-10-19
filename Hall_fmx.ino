f4a458546536d49e83bd619d7fad9e3a584029c9
Fernando Saldivar Ceron <42698132+fsaldivar@users.noreply.github.com>
2018-10-19 09:39:01 -0500

    Add files via upload

---
+++ Hall_fmx.ino
@@ -0,0 +1,24 @@
+int  sensorPin  =  A0;     // select the input  pin for  the potentiometer 
+int  ledPin  =  13;   // select the pin for  the LED
+int  sensorValue =  0;  // variable to  store  the value  coming  from  the sensor
+int led2 = 0;
+
+void setup()
+{
+pinMode(ledPin,OUTPUT);
+Serial.begin(9600);
+}
+void loop(){
+sensorValue =  analogRead(sensorPin);
+if (sensorValue > 520 )
+{
+digitalWrite(ledPin,  HIGH); 
+}
+else
+{
+digitalWrite(ledPin,  LOW); 
+}
+
+delay(100); 
+Serial.println(sensorValue);
+}
